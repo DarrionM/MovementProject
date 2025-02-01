e96e6c05d011a2405a60244b5fac680123a60424
Darrion Munroe <darrionrfm@gmail.com>
2025-02-01 13:01:01 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,57 @@
+function love.load()
+    anim8 = require 'Libraries/anim8'
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    player = {}
+    player.x = 50
+    player.y = 70
+    player.speed = 2
+    player.spriteSheet = love.graphics.newImage('Sprites/spritesheet.png')
+    player.grid = anim8.newGrid(32, 32, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
+
+    player.animations = {}
+    player.animations.idle = anim8.newAnimation(player.grid('1-10', 1), 0.2)
+
+    background = love.graphics.newImage('Sprites/background.jpg')
+end
+
+-- love.update runs every frame
+function love.update(dt)
+    right = (love.keyboard.isDown("right") or love.keyboard.isDown("d"))
+    left = (love.keyboard.isDown("left") or love.keyboard.isDown("a"))
+    up = (love.keyboard.isDown("up") or love.keyboard.isDown("w"))
+    down = (love.keyboard.isDown("down") or love.keyboard.isDown("s"))
+    speed_diag = math.sqrt((player.speed ^ 2 ) / 2)
+
+    if right and up then
+        player.x = player.x + speed_diag
+        player.y = player.y - speed_diag
+    elseif left and up then
+        player.x = player.x - speed_diag
+        player.y = player.y - speed_diag
+    elseif right and down then
+        player.x = player.x + speed_diag
+        player.y = player.y + speed_diag
+    elseif left and down then
+        player.x = player.x - speed_diag
+        player.y = player.y + speed_diag
+    elseif right then
+        player.x = player.x + player.speed
+    elseif left then
+        player.x = player.x - player.speed
+    elseif up then
+        player.y = player.y - player.speed
+    elseif down then
+        player.y = player.y + player.speed
+    end
+    player.animations.idle:update(dt) 
+end
+
+function width()
+    return 100
+end
+
+function love.draw()
+    love.graphics.draw(background, 0, 0)
+    player.animations.idle:draw(player.spriteSheet, player.x, player.y, nil, 2.25)
+end
